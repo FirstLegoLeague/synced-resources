e68296acc955fa32bee69ffe457d8e151f4e59d8
Idan Stark <idanstark42@gmail.com>
2019-05-15 10:07:56 +0300

    abstract server and collection server at 100%

--- test/lib/server/mongo/collection_server.test.js
+++ test/lib/server/mongo/collection_server.test.js
@@ -3,11 +3,14 @@
 
 const express = require('express')
 const chai = require('chai')
+const chaiSpies = require('chai-spies')
 const chaiString = require('chai-string')
 const request = require('supertest')
 const proxyquire = require('proxyquire')
+const Promise = require('bluebird')
 
 chai.use(chaiString)
+chai.use(chaiSpies)
 const expect = chai.expect
 
 const { ModelMock } = require('../../../mocks/model.mock')
@@ -339,5 +342,95 @@ describe('mongo collection server', () => {
           .expect(500, done)
       })
     })
+  })
+
+  describe('with options', () => {
+    const app = express()
+    const options = {
+      exclude: ['search', 'get'],
+      before: {
+        count: chai.spy((req, res, next) => next())
+      },
+      override: {
+        deleteAll: chai.spy((req, res, next) => res.sendStatus(200))
+      }
+    }
+    const server = new MongoCollectionServer(ModelMock, options)
+    app.use(server)
+
+    beforeEach(() => {
+      server._connectionPromise = undefined
+      MongoClient.errorsEnabled = false
+    })
+
+    it('excludes all routes in options.exclude', done => {
+      request(app)
+        .get('/search')
+        .expect(404, done)
+    })
+
+    it('calls before function of routes in options.before', done => {
+      request(app)
+        .get('/count')
+        .expect(200, () => {
+          expect(options.before.count).to.have.been.called()
+          done()
+        })
+    })
+
+    it('calls override function of routes in options.override, and does not call the original', done => {
+      MongoClient.collection.deleteMany = chai.spy(() => Promise.resolve())
+      request(app)
+        .delete('/')
+        .expect(200, () => {
+          expect(options.override.deleteAll).to.have.been.called()
+          expect(MongoClient.collection.deleteMany).not.to.have.been.called.with({})
+          done()
+        })
+    })
+  })
+
+  describe('with extendable', () => {
+    const app = express()
+    const options = {
+      extendable: true
+    }
+    const server = new MongoCollectionServer(ModelMock, options)
+    app.use(server)
+
+    beforeEach(() => {
+      server._connectionPromise = undefined
+      MongoClient.errorsEnabled = false
+    })
+
+    it('has no routes if close has not been called', done => {
+      request(app)
+        .get('/')
+        .expect(404, done)
+    })
+
+    it('has routes if close has been called', done => {
+      server.close()
+      request(app)
+        .get('/')
+        .expect(200, done)
+    })
+  })
+
+  it('saves the connection promise', done => {
+    const app = express()
+    const server = new MongoCollectionServer(ModelMock)
+    app.use(server)
+
+    request(app)
+      .get('/')
+      .expect(200, () => {
+        request(app)
+          .get('/')
+          .expect(200, () => {
+            expect(MongoClient.connect).to.have.been.called.exactly(1)
+            done()
+          })
+      })
   })
 })
