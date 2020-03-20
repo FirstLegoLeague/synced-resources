7ab671b377332e94a6a9e5282b2ef9a06e2b3d18
Idan Stark <idanstark42@gmail.com>
2020-03-20 15:57:05 +0200

    working

--- browser.js
+++ browser.js
@@ -1,13 +1,24 @@
-/* eslint import/no-dynamic-require: 0 */
-
 const { Model } = require('./lib/resources/model')
 const { ConfigurableModel } = require('./lib/resources/configurable_model')
 const { FieldsModel } = require('./lib/resources/fields_model')
 const { InvalidEntry } = require('./lib/resources/errors/invalid_entry')
 
-const clientsDir = `./lib/client${process.env.NODE_ENV === 'development' ? '/development' : ''}`
-const { CollectionClient } = require(`${clientsDir}/collection_client`)
-const { EntityClient } = require(`${clientsDir}/entity_client`)
+const { CollectionClient } = require('./lib/client/collection_client')
+const { EntityClient } = require('./lib/client/entity_client')
+
+const { DevelopmentCollectionClient } = require('./lib/client/development/collection_client')
+const { DevelopmentEntityClient } = require('./lib/client/development/entity_client')
+
 const { allowModelConfiguration } = require('./lib/client/configurable_model_client')
 
-Object.assign(exports, { Model, ConfigurableModel, FieldsModel, InvalidEntry, CollectionClient, EntityClient, allowModelConfiguration })
+Object.assign(exports, {
+  Model,
+  ConfigurableModel,
+  FieldsModel,
+  InvalidEntry,
+  CollectionClient,
+  EntityClient,
+  DevelopmentCollectionClient,
+  DevelopmentEntityClient,
+  allowModelConfiguration
+})
--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { ConfigurableModel } = require('./lib/resources/configurable_model')
 const { FieldsModel } = require('./lib/resources/fields_model')
 const { InvalidEntry } = require('./lib/resources/errors/invalid_entry')
 
+const { CollectionClient } = require('./lib/client/collection_client')
+const { EntityClient } = require('./lib/client/entity_client')
+
+const { DevelopmentCollectionClient } = require('./lib/client/development/collection_client')
+const { DevelopmentEntityClient } = require('./lib/client/development/entity_client')
+
 const { MongoCollectionServer } = require('./lib/server/mongo/collection_server')
 const { MongoEntityServer } = require('./lib/server/mongo/entity_server')
 const { MongoResourceAdapter } = require('./lib/server/mongo/resource_adapter')
@@ -14,4 +20,17 @@ const { allowModelConfiguration } = require('./lib/server/configurable_model_ser
 exports.Model = Model
 exports.InvalidEntry = InvalidEntry
 
-Object.assign(exports, { Model, ConfigurableModel, FieldsModel, InvalidEntry, MongoCollectionServer, MongoResourceAdapter, MongoEntityServer, allowModelConfiguration })
+Object.assign(exports, {
+  Model,
+  ConfigurableModel,
+  FieldsModel,
+  InvalidEntry,
+  CollectionClient,
+  EntityClient,
+  DevelopmentCollectionClient,
+  DevelopmentEntityClient,
+  MongoCollectionServer,
+  MongoResourceAdapter,
+  MongoEntityServer,
+  allowModelConfiguration
+})
